8a4a6e337197d118e925ce0e34dcf7d9d33457ae
Dharmaraj Jena <113529989+codespike9@users.noreply.github.com>
2023-11-24 21:08:28 +0530

    Update settings.py

--- GDSCBACKEND/GDSCBACKEND/settings.py
+++ GDSCBACKEND/GDSCBACKEND/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
-EMAIL_HOST_USER="dharmarajjena694@gmail.com"
-EMAIL_HOST_PASSWORD="sfumigyopzqibmvm"
+EMAIL_HOST_USER="host mail id"
+EMAIL_HOST_PASSWORD="host password"
 EMAIL_USE_TLS=True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
